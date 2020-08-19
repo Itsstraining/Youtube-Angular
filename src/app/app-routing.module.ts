716217bf82b3fa4599c1021658c7ab1d501225d2
NguyenNguyen <nguyen131519@gmail.com>
2020-08-19 15:46:32 +0700

    Done Trending

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TrendingComponent } from './components/trending/trending.component';
 import { LibraryComponent } from './components/library/library.component';
+import {VideoTrendingComponent} from './components/trending/video-trending/video-trending.component'
 
 const routes: Routes = [
   {
@@ -9,8 +10,13 @@ const routes: Routes = [
   },
   {
   path:'trending',component:TrendingComponent,
-
-}
+  children: [
+    {
+      path: ':video-trending',
+      component: VideoTrendingComponent
+    }
+  ]
+  },
 ];
 
 @NgModule({
