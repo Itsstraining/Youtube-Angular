c943ba53c889e631aa37c0cf59d963a711404653
TruongKhangLe <kangkedd@gmail.com>
2020-08-21 16:58:54 +0700

    hii

--- src/app/components/library/library.component.ts
+++ src/app/components/library/library.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../../../app/services/data.service';
-
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../nav-bar/login/login.component';
 @Component({
   templateUrl: './library.component.html',
   styleUrls: ['./library.component.scss']
 })
 export class LibraryComponent implements OnInit {
 
-  constructor(public dataService:DataService) { }
+  constructor(public dataService:DataService,private dialog:MatDialog,
+    public router: Router) { }
 
   ngOnInit(): void {
   }
-
+  openDialog() {
+    this.dialog.open(LoginComponent, {
+      width: '500px',
+      height: '500px'
+    });
+}
 }
--- src/app/components/subcriptions/subcriptions.component.ts
+++ src/app/components/subcriptions/subcriptions.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../../app/services/data.service';
+import { Router } from '@angular/router';
+
+import { MatDialog } from '@angular/material/dialog';
+import { LoginComponent } from '../nav-bar/login/login.component';
 
 @Component({
   templateUrl: './subcriptions.component.html',
@@ -7,9 +11,15 @@ import { DataService } from '../../../app/services/data.service';
 })
 export class SubcriptionsComponent implements OnInit {
 
-  constructor(public dataService:DataService) { }
+  constructor(public dataService:DataService,private dialog:MatDialog,
+    public router: Router) { }
 
   ngOnInit(): void {
   }
-
+  openDialog() {
+    this.dialog.open(LoginComponent, {
+      width: '500px',
+      height: '500px'
+    });
+}
 }
